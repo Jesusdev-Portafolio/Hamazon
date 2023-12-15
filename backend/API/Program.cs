626f9d0b5babc40dcaeb63a18d447fb2bb8eb964
Jesusdev-Portafolio <jesusdev.portafolio@gmail.com>
2023-12-15 22:53:13 +0100

    Separating Backend from Frontend and spliting apps so now we got better scafolding and theres no nested things - all working

---
+++ backend/API/Program.cs
@@ -0,0 +1,92 @@
+    using API.Extensions;
+using API.Helpers;
+using API.Middleware;
+using Core.Entities.Identity;
+using Infraestructure.Data;
+using Infraestructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using StackExchange.Redis;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//agg services al contenedor
+
+builder.Services.AddAutoMapper(typeof(MappingProfiles));
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(c => 
+c.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<AppIdentityDbContext>(c =>
+    c.UseNpgsql(builder.Configuration.GetConnectionString("IdentityConnection"))
+);
+
+//redis para el carrito singleton para que exista mientras este iniciada la aplicacion, si se apaga la aplicacion no pasa nada porque hace snapshoots cada minuto bueno
+//se perderia lo de cada minuto obviamente
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(c => {
+    var configuration = ConfigurationOptions.Parse(builder.Configuration
+        .GetConnectionString("Redis"), true);
+    return ConnectionMultiplexer.Connect(configuration);
+});
+
+builder.Services.AddApplicationServices();
+builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddSwaggerDocumentation();
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+
+    });
+});
+
+//pipeline http request
+
+var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseStaticFiles();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UserSwaggerDocumentation();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+try
+{
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context, loggerFactory);
+    var userManager = services.GetRequiredService<UserManager<User>>();
+    var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+
+    await identityContext.Database.MigrateAsync();
+    await AppIdentityDbContextSeed.SeedUserAsync(userManager);
+}
+catch (Exception ex)
+{
+  var logger = loggerFactory.CreateLogger<Program>();
+  logger.LogError(ex, "Un error ha ocurrido mientras se hacia la migracion");
+}
+
+await app.RunAsync();
+
+public partial class Program { }
--- client/src/app/shared/models/pagination.ts
+++
@@ -1,9 +0,0 @@
-import { IProduct } from "./product";
-
-export interface IPagination {
-    pageIndex: number;
-    pageSize: number;
-    totalItems: number;
-    totalPAges: number;
-    data: IProduct[];
-}
