63e168545a3ed0c765c1d67c5f2041d9ac7f7f16
Jesusdev-Portafolio <jesusdev.portafolio@gmail.com>
2024-01-09 22:35:27 +0100

    adding delivery and shipping to CustomerBasketDto and adding a server Controller to see the server time and see if the token is still valid

---
+++ backend/API/Controllers/ServerController.cs
@@ -0,0 +1,16 @@
+﻿
+
+using API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ServerController : BaseApiController
+    {
+        [HttpGet]
+        public ActionResult<UserDto> GetCurrentDate() => Ok(DateTime.Now);
+        
+    }
+}
